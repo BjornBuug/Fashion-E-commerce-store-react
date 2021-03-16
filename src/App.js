f4b550f7645df04b5e7c6621f817df7ce9e3f84f
tayarthouail <tayarthouail@gmail.com>
2021-03-16 16:26:06 +0100

    New collections work

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import Newcollections from './components/Newcollections/Newcollections'
 
 
 
@@ -11,6 +12,7 @@ function App() {
     <Navbar/>
     <Sidebar/>
     <Hero/>
+    <Newcollections/>
     </>
   );
 }
---
+++ src/components/Newcollections/Newcollections.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import model1 from '../../images/Model1.jpg';
+import model2 from '../../images/Model2.jpg';
+import model3 from '../../images/Model3.jpg';
+import model4 from '../../images/Model4.jpg';
+import model5 from '../../images/Model5.jpg';
+import model6 from '../../images/Model6.jpg';
+import model7 from '../../images/Model7.jpg';
+import model8 from '../../images/Model8.jpg';
+
+import  './Newcollections.css';
+
+
+const Newcollections = () => {
+    return (
+    <section className="section-b">
+        <h1>New Collections</h1>
+       <div className="cards">
+        <div className="card">
+            <div className="imgBox">
+                <img src={model1} alt="model"/>
+            </div>
+
+            <div className="contentBox">
+                <h3>Pants</h3>
+                <h2 className="price">$20.<small>99</small></h2>
+                <a href="#" className="buy">Buy Now</a>
+            </div>
+        </div>
+
+        <div className="card">
+            <div className="imgBox">
+                <img src={model5} alt="model"/>
+            </div>
+
+            <div className="contentBox">
+                <h3>Spring Dress</h3>
+                <h2 className="price">$30.<small>99</small></h2>
+                <a href="#" className="buy">Buy Now</a>
+            </div>
+        </div>
+
+        <div class="collections-text">
+                  <h1> Hello Spring</h1>
+                  <p>Summer 21 offers an iconic wardrobe so varied that the Tara woman can explore all of its facets, experience them, and become them. Lace collars, scallop edges, eyelet embroidery flirt with each other.
+                       The sparkle of a jewel and cascading lurex, rhinestones and lamés mix with bourgeois checks, in the continuity of winter on oversized jackets or blazers with a crest. High-waisted flared pants, pointed collars and midi dresses push a resolutely 70s style.
+                       Prints are meant to be fresh on light backgrounds suggesting sunny day.
+                       </p>
+        </div>
+        </div>
+
+        <div className="cards">
+        <div className="card">
+            <div className="imgBox">
+                <img src={model2} alt="model"/>
+            </div>
+
+            <div className="contentBox">
+                <h3>Dress Hats</h3>
+                <h2 className="price">$50.<small>99</small></h2>
+                <a href="#" className="buy">Buy Now</a>
+            </div>
+        </div>
+
+        <div className="card">
+            <div className="imgBox">
+                <img src={model3} alt="model"/>
+            </div>
+
+            <div className="contentBox">
+                <h3>Shirt</h3>
+                <h2 className="price">$15.<small>99</small></h2>
+                <a href="#" className="buy">Buy Now</a>
+            </div>
+        </div>
+
+        <div className="card">
+            <div className="imgBox">
+                <img src={model4} alt="model"/>
+            </div>
+            <div className="contentBox">
+                <h3>T-shirt</h3>
+                <h2 className="price">$21.<small>99</small></h2>
+                <a href="#" className="buy">Buy Now</a>
+            </div>
+        </div>
+
+        </div>
+
+        <div className="cards">
+        <div className="card">
+            <div className="imgBox">
+                <img src={model8} alt="model"/>
+            </div>
+
+            <div className="contentBox">
+                <h3>Jeans</h3>
+                <h2 className="price">$50.<small>99</small></h2>
+                <a href="#" className="buy">Buy Now</a>
+            </div>
+        </div>
+
+        <div className="card">
+            <div className="imgBox">
+                <img src={model6} alt="model"/>
+            </div>
+
+            <div className="contentBox">
+                <h3>Dresses</h3>
+                <h2 className="price">$15.<small>99</small></h2>
+                <a href="#" className="buy">Buy Now</a>
+            </div>
+        </div>
+
+        <div className="card">
+            <div className="imgBox">
+                <img src={model7} alt="model"/>
+            </div>
+            <div className="contentBox">
+                <h3>T-shirt & Jacket</h3>
+                <h2 className="price">$21.<small>99</small></h2>
+                <a href="#" className="buy">Buy Now</a>
+            </div>
+        </div>
+
+        </div>
+
+    </section>  
+    )
+}
+
+export default Newcollections
